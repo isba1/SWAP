13e3956ea7fc519dac9a03bb51872d3cdfd8466b
fnguyenn <fnguyen3@ucsc.edu>
2023-10-17 15:01:23 -0700

    Added Title but it looks not Fancy

---
+++ loginsrc/src/components/Title.js
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from '../style.module.css';
+
+function Title(){
+    return (<div className={styles.title}>
+            <h1 className={styles.name}>Swap Wear Apparel Platform</h1>
+        </div>);
+}
+
+export default Title;
