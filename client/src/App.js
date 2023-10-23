9ae5d512c3de3e845e02aabddeb60536a233f077
fnguyenn <fnguyen3@ucsc.edu>
2023-10-23 15:36:22 -0700

    Added Homescreen with PostUI, CHANGED ROUTE TO /home

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Account from './Login/Account';
-import Post from "./PostUI/Post";
+//import Post from "./PostUI/Post";
+import Homescreen from './Home/Homescreen';
 
 function App() {
   return (
       <Router>
           <Routes>
               <Route path="/login" element={<Account/>} />
-              <Route path="/post" element={<Post/>} />
+              <Route path="/home" element={<Homescreen/>} />
               {/* Add more routes as needed */}
           </Routes>
       </Router>
