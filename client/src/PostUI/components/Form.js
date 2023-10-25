02b7afa097eabd7b6364feddb54ba6dfaece70c3
fnguyenn <fnguyen3@ucsc.edu>
2023-10-24 23:36:02 -0700

    Did byte array but bad scope

--- client/src/PostUI/components/Form.js
+++ client/src/PostUI/components/Form.js
@@ -9,8 +9,44 @@ const Form = ({toggle, selectedFiles ,setSelectedFiles}) =>{
     const [category, setCategory] = useState('');
     const [brand, setBrand] = useState('');
 
+    const convertImagesToByteArrays = (imageFiles) => {
+        const promises = [];
+      
+        for (const file of imageFiles) {
+          promises.push(new Promise((resolve, reject) => {
+            const reader = new FileReader();
+      
+            reader.onload = (event) => {
+              const dataUrl = event.target.result;
+              const byteString = atob(dataUrl.split(',')[1]);
+              const byteArray = new Uint8Array(byteString.length);
+      
+              for (let i = 0; i < byteString.length; i++) {
+                byteArray[i] = byteString.charCodeAt(i);
+              }
+      
+              resolve(byteArray);
+            };
+      
+            reader.readAsDataURL(file);
+          }));
+        }
+      
+        return Promise.all(promises);
+      };
+
+
     const handleSubmit = (event) =>{
         event.preventDefault();
+        convertImagesToByteArrays(selectedFiles)
+        .then(byteArrays => {
+          const imageByteArrays = byteArrays;
+          console.log(imageByteArrays);
+          //THIS IS WHERE THE BYTE ARRAY IS, THE SCOPE CAN'T GO OUT OF THIS
+        })
+        .catch(error => {
+          console.error(error);
+        });
         console.log(productName, description, category, brand);
         //AXIOS HERE, PICTURE FILES ARE IN "selectedFiles" array
         setProductName('');
