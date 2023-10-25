f7fc6cffe561e5847d3826ddd392bc2b08773b49
Hemal61 <71044815+Hemal61@users.noreply.github.com>
2023-10-24 22:49:02 -0700

    Interest selectiopm has endpoints but I am not sure where to connect them

--- client/src/Login/components/InterestSelection.js
+++ client/src/Login/components/InterestSelection.js
@@ -1,7 +1,28 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import styles from '../style.module.css';
 
+
 const InterestSelection = (props) => {
+    const [selectedStyle, setSelectedStyle] = useState(''); // Initialize the selectedStyle state
+    const navigate = useNavigate();
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        const userPreferenceRequestBody = {
+            clothingStyle: selectedStyle // Only the selected clothing style
+        }
+
+        try {
+            await axios.post(`http://localhost:8080/login/saveAccountInfo`, userPreferenceRequestBody);
+            // navigate('/post'); --> If you have a navigate function
+        } catch (error) {
+            console.error(error);
+        }
+
+        props.onFormSwitch('passwordCreation');
+    }
+
     return (
         <div className={styles.formcontainer}>
             <h1 className={styles.header}>Tell Us About Your Fashion Interests</h1>
@@ -11,7 +32,7 @@ const InterestSelection = (props) => {
                 <div>
                     {/* List out the clothing styles, make sure to add or remove as needed */}
                     {["Casual", "Formal", "Vintage", "Streetwear", "Bohemian", "Preppy", "Androgynous", "Goth", "Hipster"].map(style => (
-                        <button className={styles.button} key={style}>{style}</button>
+                        <button className={styles.button} key={style}>{style} onClick={() => setSelectedStyle(style)}</button>
                     ))}
                 </div>
 
