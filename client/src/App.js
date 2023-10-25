022e893e3015273d4319db747dc160f24bb64b59
Ishaan Das <95513685+isba1@users.noreply.github.com>
2023-10-24 21:33:00 -0700

    added interests route

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Account from './Login/Account';
 //import Post from "./PostUI/Post";
 import Homescreen from './Home/Homescreen';
+import InterestSelection from "./Login/components/InterestSelection";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
           <Routes>
               <Route path="/login" element={<Account/>} />
               <Route path="/home" element={<Homescreen/>} />
+              <Route path="/newUserInterests" element={<InterestSelection/>} />
               {/* Add more routes as needed */}
           </Routes>
       </Router>
