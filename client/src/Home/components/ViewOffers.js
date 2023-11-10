ad95cec66eadc7f6c5b17a658ab832ec1e44291e
preeths-r <prangamg@ucsc.edu>
2023-11-10 00:37:39 -0800

    accept and decline button

--- client/src/Home/components/ViewOffers.js
+++ client/src/Home/components/ViewOffers.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import "./homescreen.css";
 
 function ViewOffersButton({ onClick }) {
   const [isPopupOpen, setPopupOpen] = useState(false);
@@ -12,7 +13,6 @@ function ViewOffersButton({ onClick }) {
 
   const fetchTradeOffers = async () => {
     try {
-      // Replace 'YOUR_USER_ID' with the actual user ID or fetch it from your authentication state
       const userID = "YOUR_USER_ID";
 
       // Fetch trade offers from the backend
@@ -25,10 +25,40 @@ function ViewOffersButton({ onClick }) {
     }
   };
 
+  const acceptOffer = async (offer) => {
+    try {
+      console.log("Offer Accepted:", offer);
+      const response = await axios.get(
+        `http://localhost:8080/offer/acceptOffer?sellerPostID=${offer.sellerPostID}&sellerUserID=${offer.sellerUserID}&buyerPostID=${offer.buyerPostID}&buyerUserID=${offer.buyerUserID}`
+      );
+      console.log("Accept Offer Response:", response.data);
+      // After accepting, you may want to refetch the trade offers
+      fetchTradeOffers();
+    } catch (error) {
+      console.error("Error accepting offer:", error);
+    }
+  };
+
+  const declineOffer = async (offer) => {
+    try {
+      console.log("Offer Declined:", offer);
+      const response = await axios.get(
+        `http://localhost:8080/offer/declineOffer?sellerPostID=${offer.sellerPostID}&sellerUserID=${offer.sellerUserID}&buyerPostID=${offer.buyerPostID}&buyerUserID=${offer.buyerUserID}`
+      );
+      console.log("Decline Offer Response:", response.data);
+      // After declining, you may want to refetch the trade offers
+      fetchTradeOffers();
+    } catch (error) {
+      console.error("Error declining offer:", error);
+    }
+  };
+
   const togglePopup = () => {
     setPopupOpen(!isPopupOpen);
   };
+
   console.log(tradeOffers);
+
   return (
     <div>
       <button className="view-offers-button" onClick={togglePopup}>
@@ -44,10 +74,9 @@ function ViewOffersButton({ onClick }) {
                 {/* Display offer details */}
                 <p>Name of Your Product: {offer.myName}</p>
                 <img
-                  src={`data:image/png;base64, ${offer.myBase64Images[0]}`} // Assuming the first image, adjust accordingly
+                  src={`data:image/png;base64, ${offer.myBase64Images[0]}`}
                   alt="Your Product"
                 />
-
                 <p>Your Product Description: {offer.myPostDescription}</p>
                 <p>Your Product Category: {offer.myPostCategory}</p>
                 <p>Your Product Brand: {offer.myPostBrand}</p>
@@ -56,16 +85,19 @@ function ViewOffersButton({ onClick }) {
 
                 <p>Name of Their Product: {offer.theirUserName}</p>
                 <img
-                  src={`data:image/png;base64, ${offer.theirBase64Images[0]}`} // Assuming the first image, adjust accordingly
+                  src={`data:image/png;base64, ${offer.theirBase64Images[0]}`}
                   alt="Their Product"
                 />
-
                 <p>Their Product Description: {offer.theirPostDescription}</p>
                 <p>Their Product Category: {offer.theirPostCategory}</p>
                 <p>Their Product Brand: {offer.theirPostBrand}</p>
                 <p>Their Product Style: {offer.theirPostStyle}</p>
                 <p>Their Product Size: {offer.theirPostSize}</p>
-                {/* Add more details as needed */}
+
+                <button onClick={() => acceptOffer(offer)}>Accept Offer</button>
+                <button onClick={() => declineOffer(offer)}>
+                  Decline Offer
+                </button>
               </div>
             ))}
             <button onClick={togglePopup}>Close</button>
