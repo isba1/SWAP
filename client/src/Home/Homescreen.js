845ece0d3dc59b3206590f57b66596e939451809
Hemal Bhaveen <hemalbhaveen@Hemals-MacBook-Pro.local>
2023-11-28 11:46:34 -0800

    changed userid to username and finihsed recommended posts

--- client/src/Home/Homescreen.js
+++ client/src/Home/Homescreen.js
@@ -3,42 +3,37 @@ import { Suspense } from 'react';
 import "./components/homescreen.css";
 import SideBar from './components/SideBar';
 import HomeBar from './components/Homebar';
+import axios from 'axios'
 
 const LazyHomePosts = React.lazy(() => import('./components/HomePosts'));
 
 const Homescreen = () => {
   const [loadedPosts, setLoadedPosts] = useState(0);
+  const [recommendedPosts, setRecommendedPosts] = useState([]);
   const postsRef = useRef(null);
 
   //this will be the myuser's userid/token
-  const userID = sessionStorage.getItem("userName");
+  const userNAME = sessionStorage.getItem("userNAME");
   //this will be a FeedUserPost object
-  
-  const FeedUserTest = {
-    postID: '',
-    userID: '',
-    userName: 'ExampleUser',
-    name: 'Gray Sweatpants',
-    postDescription: 'Gray Sweapants',
-    postCategory: 'EXAMPLE',
-    postBrand: 'EXAMPLE',
-    postStyle: 'EXAMPLE',
-    postSize: 'EXAMPLE',
-  }
-  
+
   useEffect(() => {
+    axios.get(`/api/recommendedPosts/${Home}`) //  API endpoint goes here
+      .then(response => {
+        setRecommendedPosts(response.data);
+      })
+      .catch(error => {
+        console.error('Error fetching recommended posts:', error);
+      });
+
     const observer = new IntersectionObserver((entries) => {
       if (entries[0].isIntersecting) {
-        //console.log('Element is intersecting');
         setLoadedPosts((prevLoadedPosts) => prevLoadedPosts + 1);
-      } else {
-        //console.log('Element is not intersecting');
       }
     });
 
     observer.observe(postsRef.current);
     return () => observer.disconnect();
-  }, []);
+  }, [userNAME]);
 
   return (
     <div className='homerow'>
@@ -47,10 +42,20 @@ const Homescreen = () => {
       </div>
       <div className='homecolumnright'>
         <HomeBar />
-        {Array.from({ length: loadedPosts }).map((_, index) => (
-          <Suspense key={index} fallback={<div>Loading post...</div>}>
-            <LazyHomePosts FeedUserPostObject={FeedUserTest} UserID={userID}/>
-          </Suspense>
+        {recommendedPosts.slice(0, loadedPosts).map((post, index) => (
+          <div key={index}>
+            <Suspense fallback={<div>Loading post...</div>}>
+              <LazyHomePosts FeedUserPostObject={post} UserNAME={userNAME} />
+            </Suspense>
+            <div className="post-details">
+              <h3>{post.userName}</h3>
+              <p>{post.postDescription}</p>
+              <p>Category: {post.postCategory}</p>
+              <p>Brand: {post.postBrand}</p>
+              <p>Style: {post.postStyle}</p>
+              <p>Size: {post.postSize}</p>
+            </div>
+          </div>
         ))}
         <div ref={postsRef}></div>
       </div>
