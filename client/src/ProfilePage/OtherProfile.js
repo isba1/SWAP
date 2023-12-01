4dc5109ee9491f4d1a53a9dce97c1fffb874efb3
preeths-r <prangamg@ucsc.edu>
2023-12-01 14:04:20 -0800

    follow button endpoints added

--- client/src/ProfilePage/OtherProfile.js
+++ client/src/ProfilePage/OtherProfile.js
@@ -1,84 +1,135 @@
-import React, { useState, useRef, useEffect } from 'react';
-import axios from 'axios';
-import { Suspense } from 'react';
+import React, { useState, useRef, useEffect } from "react";
+import axios from "axios";
+import { Suspense } from "react";
 import "./profile.css";
-import { useNavigate, useParams } from 'react-router-dom';
+import { useNavigate, useParams } from "react-router-dom";
 
-const LazyProfilePosts = React.lazy(() => import('./OtherProfilePosts'));
+const LazyProfilePosts = React.lazy(() => import("./OtherProfilePosts"));
 
-function ProfilePage (){
-    const navigate = useNavigate();
-    const userID = sessionStorage.getItem("userName");
-    const { username } = useParams();
-    if (userID === username){
-      navigate('/myprofile');
+function ProfilePage() {
+  const navigate = useNavigate();
+  const userID = sessionStorage.getItem("userName");
+  const { username } = useParams();
+  if (userID === username) {
+    navigate("/myprofile");
+  }
+  const [loadedPosts, setLoadedPosts] = useState(0);
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const postsRef = useRef(null);
+  const [posts, setPosts] = useState([]);
+  const [totalPosts, setTotalPosts] = useState(0);
+  const [followed, setFollowed] = useState(false);
+
+  const fetchFollowStatus = async () => {
+    try {
+      const response = await axios.get(
+        `http://localhost:8080/profile/singleFollower?userID=${userID}`
+      );
+      setFollowed(response.data.followed);
+    } catch (error) {
+      console.error("Error fetching follow status:", error);
     }
-    const [loadedPosts, setLoadedPosts] = useState(0);
-    const [data, setData] = useState(null);
-    const [loading, setLoading] = useState(true);
-    const postsRef = useRef(null);
-    const [posts, setPosts] = useState([]);
-    const [totalPosts, setTotalPosts] = useState(0);
+  };
 
-    const handleChange = async (event) => {
-        navigate('/home');
-    }
+  const handleChange = async (event) => {
+    navigate("/home");
+  };
 
-    useEffect(() =>{
-        if (data === null){
-            axios.get(`http://localhost:8080/search/singleUser?userID=${username}`)
-            .then((response) => {
-                setData(response.data);
-                setPosts(response.data.userPosts);
-                setTotalPosts(response.data.userPosts.length);
-                setLoading(false);
-            })
-            .catch((error) => {
-                console.error('Error fetching data:', error);
-                setLoading(false);
-            });
-        }
-    }, [data, username]);
-
-    useEffect(() => {
-        const loadMorePosts = () =>{
-            if (loadedPosts < posts.length){
-                setLoadedPosts((prevLoadedPosts) => prevLoadedPosts + 1);
-            }
-        };
-        const observer = new IntersectionObserver((entries) => {
-          if (entries[0].isIntersecting) {
-            //console.log('Element is intersecting');
-            loadMorePosts();
-          } else {
-            //console.log('Element is not intersecting');
-          }
+  useEffect(() => {
+    if (data === null) {
+      axios
+        .get(`http://localhost:8080/search/singleUser?userID=${username}`)
+        .then((response) => {
+          setData(response.data);
+          setPosts(response.data.userPosts);
+          setTotalPosts(response.data.userPosts.length);
+          setLoading(false);
+        })
+        .catch((error) => {
+          console.error("Error fetching data:", error);
+          setLoading(false);
         });
-    
-        observer.observe(postsRef.current);
-        return () => observer.disconnect();
-      }, [loadedPosts, totalPosts, posts]);
+    }
+  }, [data, username]);
 
-    return(<div>
-            <div className='profileheader'>
-            {loading ? (
-                <p>Loading...</p>
-            ) : (
-                <div className='profilehorizontalcontainer'>
-                <button className='homebutton' onClick={handleChange}>Home</button>
-                <h1>{username}</h1>
-                <p>Followers: {data.followersCount}</p>
-                <p>Following: {data.followingCount}</p>
-                </div>
-            )}
-            </div>
-            {posts.slice(0, loadedPosts).map((post, index) => (
-                <Suspense key={index} fallback={<div>Loading post...</div>}>
-                    <LazyProfilePosts PostObject={post} UserID={userID} SellerId={username}/>
-                </Suspense>
-            ))}
-        <div ref={postsRef}></div>
-    </div>)
+  useEffect(() => {
+    const loadMorePosts = () => {
+      if (loadedPosts < posts.length) {
+        setLoadedPosts((prevLoadedPosts) => prevLoadedPosts + 1);
+      }
+    };
+    const observer = new IntersectionObserver((entries) => {
+      if (entries[0].isIntersecting) {
+        //console.log('Element is intersecting');
+        loadMorePosts();
+      } else {
+        //console.log('Element is not intersecting');
+      }
+    });
+
+    observer.observe(postsRef.current);
+    return () => observer.disconnect();
+  }, [loadedPosts, totalPosts, posts]);
+
+  const handleFollowClick = async () => {
+    try {
+      setFollowed(!followed);
+
+      // Use the appropriate endpoint based on follow/unfollow action
+      if (!followed) {
+        await axios.get(
+          `http://localhost:8080/follow/add?loginUserName=${userID}&userNameToFollow=${username}`
+        );
+      } else {
+        await axios.get(
+          `http://localhost:8080/follow/remove?loginUserName=${userID}&userNameToRemoveFollow=${username}`
+        );
+      }
+
+      // Update follow status after API call
+      fetchFollowStatus();
+    } catch (error) {
+      console.error("Error toggling follow status:", error);
+      setFollowed(!followed); // Reset the state to previous value on error
+    }
+  };
+
+  return (
+    <div>
+      <div className="profileheader">
+        {loading ? (
+          <p>Loading...</p>
+        ) : (
+          <div className="profilehorizontalcontainer">
+            <button className="homebutton" onClick={handleChange}>
+              Home
+            </button>
+            <h1>{username}</h1>
+            <p>Followers: {data.followersCount}</p>
+            <p>Following: {data.followingCount}</p>
+            <button
+              className="follow"
+              onClick={handleFollowClick}
+              style={{ backgroundColor: followed ? "green" : "black" }}
+            >
+              {followed ? "Following" : "Follow"}
+            </button>
+          </div>
+        )}
+      </div>
+      {posts.slice(0, loadedPosts).map((post, index) => (
+        <Suspense key={index} fallback={<div>Loading post...</div>}>
+          <LazyProfilePosts
+            PostObject={post}
+            UserID={userID}
+            SellerId={username}
+          />
+        </Suspense>
+      ))}
+      <div ref={postsRef}></div>
+    </div>
+  );
 }
 
 export default ProfilePage;
