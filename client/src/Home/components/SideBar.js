11f37ffab1dec38385842ac53fe9c1041be314ff
Ishaan Das <95513685+isba1@users.noreply.github.com>
2023-12-03 14:28:07 -0800

    removed side bar

--- client/src/Home/components/SideBar.js
+++ client/src/Home/components/SideBar.js
@@ -11,15 +11,7 @@ function SideBar(){
     return(<div>
         <h1 className="homeheader">SWAP</h1>
         <button onClick={handleChange}>My Profile</button>
-        <h2 className="hometitle">Explore</h2>
-            <div className="homesidecontainer">
-                <button className="homeexbutton">Tops</button>
-                <button className="homeexbutton">Bottoms</button>
-                <button className="homeexbutton">Outerwear</button>
-                <button className="homeexbutton">Shoes</button>
-                <button className="homeexbutton">Accessories</button>
-                <Post/>            
-            </div>
+        <Post/>
     </div>)
 }
 
