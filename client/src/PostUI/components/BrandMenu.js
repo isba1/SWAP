3d4f2a8d19af1ba2747285ad26981c21cf7d56a7
fnguyenn <fnguyen3@ucsc.edu>
2023-12-03 11:55:24 -0800

    Fixed Profile Page and Updated Post Menus

--- client/src/PostUI/components/BrandMenu.js
+++ client/src/PostUI/components/BrandMenu.js
@@ -12,6 +12,12 @@ function BrandMenu({selectedOption, setSelectedOption}) {
       <option value="" disabled>Choose a Brand</option>
         <option value="Nike">Nike</option>
         <option value="Adidas">Adidas</option>
+        <option value="Puma">Puma</option>
+        <option value="Under Armor">Under Armor</option>
+        <option value="Ralph Lauren">Ralph Lauren</option>
+        <option value="Tommy Hilfiger">Tommy Hilfiger</option>
+        <option value="Patagonia">Patagonia</option>
+        <option value="Lacoste">Lacoste</option>
         <option value="Levi's">Levi's</option>
         <option value="Calvin Klein">Calvin Klein</option>
         <option value="Other">Other</option>
