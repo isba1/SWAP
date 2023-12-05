507d12a87ae2a63c36cd7fdebe5bd692882d2946
fnguyenn <fnguyen3@ucsc.edu>
2023-12-04 18:26:14 -0800

    Added Side Button Functionality

--- client/src/Home/components/SideBar.js
+++ client/src/Home/components/SideBar.js
@@ -2,17 +2,66 @@ import React from "react";
 import "./homescreen.css";
 import Post from "../../PostUI/Post";
 import { useNavigate } from 'react-router-dom';
+import axios from "axios";
 
 function SideBar({setFilteredPosts}){
+    const userID = sessionStorage.getItem("userName");
     const navigate = useNavigate();
     const handleChange = async (event) => {
         navigate('/myprofile');
     }
 
     const clearFilteredPosts = () =>{
-        setFilteredPosts([]);
-        window.location.reload(true);
-      }
+        setFilteredPosts(null);
+    }
+
+    const handleTopsButtonClicked = async () =>{
+        axios.get(`http://localhost:8080/home/exploreTops?userID=${userID}`)
+        .then((response) => {
+            setFilteredPosts(response.data);
+        })
+        .catch((error) => {
+          console.error(error)
+        })
+    }
+    const handleBottomsButtonClicked = async () =>{
+        axios.get(`http://localhost:8080/home/exploreBottoms?userID=${userID}`)
+        .then((response) => {
+            setFilteredPosts(response.data);
+        })
+        .catch((error) => {
+          console.error(error);
+        })
+    }
+    const handleOuterWearButtonClicked = async ()=>{
+        axios.get(`http://localhost:8080/home/exploreOuterwear?userID=${userID}`)
+        .then((response) => {
+            setFilteredPosts(response.data);
+        })
+        .catch((error) => {
+          console.error(error);
+        })
+    }
+    const handleShoesButtonClicked = async () =>{
+        axios.get(`http://localhost:8080/home/exploreShoes?userID=${userID}`)
+        .then((response) => {
+            setFilteredPosts(response.data);
+        })
+        .catch((error) => {
+          console.error(error);
+        })
+    }
+    const handleAccessoriesButtonClicked = async () =>{
+        axios.get(`http://localhost:8080/home/exploreAccessories?userID=${userID}`)
+        .then((response) => {
+            setFilteredPosts(response.data);
+        })
+        .catch((error) => {
+          console.error(error);
+        })
+    }
+
+
 
     return(<div>
         <h1 className="homeheader">SWAP</h1>
@@ -20,11 +69,11 @@ function SideBar({setFilteredPosts}){
         <h2 className="hometitle">Explore</h2>
             <div className="homesidecontainer">
                 <button className="homeexbutton" onClick={clearFilteredPosts}>Recommended</button> 
-                <button className="homeexbutton">Tops</button>
-                <button className="homeexbutton">Bottoms</button>
-                <button className="homeexbutton">Outerwear</button>
-                <button className="homeexbutton">Shoes</button>
-                <button className="homeexbutton">Accessories</button>            
+                <button className="homeexbutton" onClick={handleTopsButtonClicked}>Tops</button>
+                <button className="homeexbutton" onClick={handleBottomsButtonClicked}>Bottoms</button>
+                <button className="homeexbutton" onClick={handleOuterWearButtonClicked}>Outerwear</button>
+                <button className="homeexbutton" onClick={handleShoesButtonClicked}>Shoes</button>
+                <button className="homeexbutton" onClick={handleAccessoriesButtonClicked}>Accessories</button>            
             </div>
         <Post/>
     </div>)
